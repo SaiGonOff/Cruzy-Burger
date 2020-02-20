e5ef4c3cbb1f464188b0d9d9d0d452c5d99a77b6
SaiGonOff <54752749+SaiGonOff@users.noreply.github.com>
2020-02-20 17:21:32 -0300

    Versao 0.0.1
    Foi adicionado JoyStick, e um prototipo de um play

---
+++ Crazy Burger/Assets/Script/Movimento.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public float Velocidade;
+    private float Vertical;
+    private float horizontal;
+    public FixedJoystick Joystick;
+
+    void Update()
+    {
+
+        horizontal = Joystick.Horizontal;
+        Vertical = Joystick.Vertical;
+        transform.Translate(new Vector3(horizontal, 0, Vertical) * Velocidade * Time.deltaTime);
+  
+    }
+}
